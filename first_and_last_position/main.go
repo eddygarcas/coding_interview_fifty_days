01dcaca1200248610980af2667df19f53ea81939
duard <eduard@rzilient.club>
2025-06-03 19:00:18 +0200

    Find the First and Last position in a sorted array

--- first_and_last_position/main.go
+++ first_and_last_position/main.go
@@ -5,18 +5,18 @@ import (
 	"time"
 )
 
-type Point struct {
+type point struct {
 	x, y int
 }
 
-func (p Point) toArray() []int {
+func (p point) ToArray() []int {
 	return []int{p.x, p.y}
 }
 
 func main() {
-	e := []int{10, 13, 11, 15, 17, 18}
-	target := 20
-	result := Point{x: -1, y: -1}
+	e := []int{10, 11, 13, 14, 11, 18}
+	target := 11
+	result := point{x: -1, y: -1}
 
 	// Find first target occurrence left to right
 	// Keep the index
@@ -27,13 +27,13 @@ func main() {
 	for index, element := range e {
 		if element == target {
 			if result.x == -1 {
-				result = Point{x: index, y: index}
+				result = point{x: index, y: index}
 			} else {
 				result.y = index
 			}
 		}
 	}
-	fmt.Printf("First and last position is %v\n", result.toArray())
+	fmt.Printf("First and last position is %v\n", result.ToArray())
 	elapsed := time.Since(start)
 	fmt.Printf("Execution time: %v\n", elapsed)
 }
