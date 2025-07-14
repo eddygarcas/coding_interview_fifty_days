bc0221bc04155d37d78c1053a2478134620b4030
duard <eduard@rzilient.club>
2025-07-14 17:31:59 +0200

    Remove nth node end list

--- 25 - remove nth node end of list/main.go
+++ 25 - remove nth node end of list/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // ListNode defines a node in a singly linked list.
-// Each node contains an integer value and a pointer to the next node in the list.
+// Each node contains an integer value and a pointer to the next node.
 type ListNode struct {
 	Val  int       // Value stored in the node
 	Next *ListNode // Pointer to the next node in the list
@@ -18,7 +18,7 @@ func main() {
 	node5 := &ListNode{Val: 5}
 	node6 := &ListNode{Val: 6}
 
-	// Link the nodes together to form a singly linked list
+	// Link the nodes together to form the list
 	node1.Next = node2
 	node2.Next = node3
 	node3.Next = node4
@@ -27,9 +27,10 @@ func main() {
 	node6.Next = nil // End of list
 
 	// Remove the 2nd node from the end (should remove node 5)
-	// Pass the head of the list and the position from the end (1-based index)
+	// Calls the method on the head node
 	result := node1
 	node1.removeNthNode(2)
+
 	// Print the modified list
 	for result != nil {
 		fmt.Printf("%d,", result.Val)
@@ -38,10 +39,10 @@ func main() {
 }
 
 // removeNthNode removes the nth node from the end of the singly linked list.
+// This is a method on the ListNode type.
 // Parameters:
-// t: Head of the linked list
 // n: The position from the end (1-based index)
-// Returns the head of the modified list.
+// Modifies the list in place. No return value.
 func (t *ListNode) removeNthNode(n int) {
 	// temp is used to count the total number of nodes in the list
 	temp := t
@@ -49,6 +50,7 @@ func (t *ListNode) removeNthNode(n int) {
 	ans := t   // Used to traverse to the node before the one to remove
 
 	// First pass: count the total number of nodes in the list
+	// Traverse the list to get the total count of nodes
 	for temp != nil {
 		index++
 		temp = temp.Next
@@ -56,10 +58,12 @@ func (t *ListNode) removeNthNode(n int) {
 
 	// Second pass: move to the node just before the one to remove
 	// We subtract n+1 from the total count to get the position from the start
+	// Traverse the list to get to the node before the one to remove
 	for l := 0; l < index-(n+1); l++ {
 		ans = ans.Next
 	}
 	// Remove the nth node from the end by adjusting pointers
 	// We set the next pointer of the node before the one to remove to nil
+	// This effectively removes the nth node from the end
 	ans.Next = nil
 }
